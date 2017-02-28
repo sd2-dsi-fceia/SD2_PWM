44d0f7ae0da63ae19eb713c981bcbde19a1c5377
Gustavo Muro <gustmuro@gmail.com>
2017-02-28 13:20:27 -0300

    se quitaron secciones que no se utilizan en este ejemplo

--- board/inc/board.h
+++ board/inc/board.h
@@ -1,3 +1,36 @@
+/* Copyright 2017, DSI FCEIA UNR - Sistemas Digitales 2
+ *    DSI: http://www.dsi.fceia.unr.edu.ar/
+ * Copyright 2017, Diego Alegrechi
+ * Copyright 2017, Gustavo Muro
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 #ifndef BOARD_H_
 #define BOARD_H_
@@ -11,21 +44,6 @@ extern "C" {
 #endif
 
 /*==================[macros]=================================================*/
-
-/** \brief puerto I2C utilizado en el acelerómetro  */
-#define MMA8451_I2C     I2C0
-
-#define UART_INSTANCE   ((UART_Type*)UART0)
-
-/* The UART to use for debug messages. */
-#ifndef BOARD_DEBUG_UART_INSTANCE
-    #define BOARD_DEBUG_UART_INSTANCE   0
-    #define BOARD_DEBUG_UART_BASEADDR   UART0
-#endif
-#ifndef BOARD_DEBUG_UART_BAUD
-    #define BOARD_DEBUG_UART_BAUD       115200
-#endif
-
 
 /*==================[typedef]================================================*/
 
--- board/src/board.c
+++ board/src/board.c
@@ -1,3 +1,36 @@
+/* Copyright 2017, DSI FCEIA UNR - Sistemas Digitales 2
+ *    DSI: http://www.dsi.fceia.unr.edu.ar/
+ * Copyright 2017, Diego Alegrechi
+ * Copyright 2017, Gustavo Muro
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 /*==================[inclusions]=============================================*/
 #include "board.h"
@@ -9,7 +42,6 @@
 #include "fsl_clock_MKL46Z4.h"
 #include "fsl_i2c_hal.h"
 #include "fsl_lpsci_hal.h"
-#include "mma8451.h"
 
 /*==================[macros and definitions]=================================*/
 
@@ -89,57 +121,6 @@ void board_init(void)
     GPIO_HAL_SetPinDir(SW3_GPIO, SW3_PIN, kGpioDigitalInput);
     PORT_HAL_SetPullCmd(SW3_PORT, SW3_PIN, true);
     PORT_HAL_SetPullMode(SW3_PORT, SW3_PIN, kPortPullUp);
-
-    /* =========== I2C =================== */
-
-	/* seleccion función alternativa 5 (I2C) */
-	PORT_HAL_SetMuxMode(PORTE, 24,kPortMuxAlt5);
-
-	/* seleccion función alternativa 5 (I2C) */
-	PORT_HAL_SetMuxMode(PORTE, 25,kPortMuxAlt5);
-
-	/* activa clock para I2C */
-	SIM_HAL_EnableClock(SIM, kSimClockGateI2c0);
-
-	/* inicializa el I2C */
-	I2C_HAL_Init(I2C0);
-
-	/* configura baudrate */
-	I2C_HAL_SetBaudRate(I2C0, SystemCoreClock, 100, NULL);
-
-	/* activa el I2C */
-	I2C_HAL_Enable(I2C0);
-
-	/* =========== MMA8451 ================ */
-
-    mma8451_init();
-
-
-    /* =========== UART ================ */
-
-    PORT_HAL_SetMuxMode(PORTA, 1u, kPortMuxAlt2);
-    PORT_HAL_SetMuxMode(PORTA, 2u, kPortMuxAlt2);
-
-
-
-    /* selecciona clock de PLLFLLSEL */
-    CLOCK_HAL_SetLpsciSrc(SIM, 0, kClockLpsciSrcPllFllSel);
-
-    /* habilita clock a la UART0 (Lpsci0) */
-    SIM_HAL_EnableClock(SIM, kSimClockGateLpsci0);
-
-    /* setea baudrate */
-    LPSCI_HAL_SetBaudRate(UART0, SystemCoreClock, 115200);
-
-    /* configura 8 bits de datos */
-    LPSCI_HAL_SetBitCountPerChar(UART0, kLpsci8BitsPerChar);
-
-    /* deshabilita paridad */
-    LPSCI_HAL_SetParityMode(UART0, kLpsciParityDisabled);
-
-    /* 1 bit de stop */
-    LPSCI_HAL_SetStopBitCount(UART0, kLpsciOneStopBit);
-
 }
 
 int8_t pulsadorSw1_get(void)
